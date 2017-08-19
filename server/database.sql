e99917bb7cd1f38e4b3c6b586faddb462b3a0c47
Sam Fortin <sam.j.fortin@gmail.com>
2017-08-18 19:34:02 -0500

    sql database updated

--- server/database.sql
+++ server/database.sql
@@ -1,9 +1,8 @@
--- Table 1: Employees
 CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(80) NOT NULL,
     last_name VARCHAR(80) NOT NULL,
     job_title VARCHAR(80) NOT NULL,
     salary INT NOT NULL,
-    isactive boolean NOT NULL
+    is_active boolean NOT NULL
 );
