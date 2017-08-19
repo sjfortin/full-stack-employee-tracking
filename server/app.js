d043e5b48c28340c31343fdcfc747970a8cafc43
Sam Fortin <sam.j.fortin@gmail.com>
2017-08-18 19:59:37 -0500

    Employees route functioning

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-// var employees = require('./routes/employees');
+var employees = require('./routes/employees');
 
 var port = 5000;
 
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
-// app.use('/employees', employees);
+app.use('/employees', employees);
 
 app.listen(port, function(){
     console.log('Running on port', port); 
